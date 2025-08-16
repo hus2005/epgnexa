87832656ae69b5d4c521df5734373146fcfa60d5
hus2005 <120742279+hus2005@users.noreply.github.com>
2025-08-16 18:21:09 +0300

    Update generate_epg.py

--- generate_epg.py
+++ generate_epg.py
@@ -1,1 +1,18 @@
 
+import requests
+import datetime
+
+# Senin kendi sunucundaki EPG kaynağı
+url = "http://178.63.255.253:2701/turkey_fixed.xml"
+
+try:
+    r = requests.get(url, timeout=60)
+    r.raise_for_status()
+
+    with open("turkey.xml", "wb") as f:
+        f.write(r.content)
+
+    print("✅ EPG güncellendi:", datetime.datetime.now())
+
+except Exception as e:
+    print("❌ Hata:", e)
